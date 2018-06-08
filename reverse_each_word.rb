9d9172e437b0ebb76f0fe737118f70949e93d7cc
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 16:29:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_array = sentence.split 
   new_array.each do |word|
-    word.reverse 
+    word.reverse!
   end 
   new_sentence = new_array.join 
   new_sentence
